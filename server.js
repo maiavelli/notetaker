5cbf7ff6bd189459513d81ad0431b1d0714439a2
maiavelli <maiacamilledavis@gmail.com>
2022-06-24 10:54:49 -0400

    server.js created and express functions made to start server

---
+++ server.js
@@ -0,0 +1,46 @@
+// dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const database = require('./db/db');
+
+// set up express app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+    console.log('app listening on PORT: ' + PORT)
+});
+
+// get homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// get notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// render existing notes to page
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+});
+
+// post method to add new notes
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data); 
+        const newNote = req.body;
+
+        notes.push(newNote);
+        
+    })
+})
+    
